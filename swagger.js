41a13b4997701974f0089d3e0a3df6e461a35f80
OnyekachiMiracleNechi <168313634+OnyekachiMiracleNechi@users.noreply.github.com>
2025-09-14 20:40:58 +0100

    new commiy

--- swagger.js
+++ swagger.js
@@ -2,14 +2,15 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'Temples API',
-    description: 'API documentation for temple routes',
+    title: ' Contacts and Temples API',
+    description: 'API documentation for contacts and temples routes',
   },
-  host: 'localhost:3000',
-  schemes: ['http'],
+  "host": "localhost:3000",
+"schemes": ["http"]
+
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js']; // adjust if your temple routes are in another file
+const endpointsFiles = ['./routes/temples.js', './routes/contacts.js']; // adjust if your temple routes are in another file
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
